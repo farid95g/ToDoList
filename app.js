d274bdd04d18fe810c1516b8d9ec55bed8b1d5a9
farid95g <f95guluzade@gmail.com>
2021-01-03 23:24:22 +0400

    dynamic theme change functionality

--- app.js
+++ app.js
@@ -37,6 +37,74 @@ const tasks = [
     return acc;
   }, {});
 
+  const themes = {
+    default: {
+      '--base-text-color': '#212529',
+      '--header-bg': '#007bff',
+      '--header-text-color': '#fff',
+      '--default-btn-bg': '#007bff',
+      '--default-btn-text-color': '#fff',
+      '--default-btn-hover-bg': '#0069d9',
+      '--default-btn-border-color': '#0069d9',
+      '--danger-btn-bg': '#dc3545',
+      '--danger-btn-text-color': '#fff',
+      '--danger-btn-hover-bg': '#bd2130',
+      '--danger-btn-border-color': '#dc3545',
+      '--input-border-color': '#ced4da',
+      '--input-bg-color': '#fff',
+      '--input-text-color': '#495057',
+      '--input-focus-bg-color': '#fff',
+      '--input-focus-text-color': '#495057',
+      '--input-focus-border-color': '#80bdff',
+      '--input-focus-box-shadow': '0 0 0 0.2rem rgba(0, 123, 255, 0.25)',
+    },
+    dark: {
+      '--base-text-color': '#212529',
+      '--header-bg': '#343a40',
+      '--header-text-color': '#fff',
+      '--default-btn-bg': '#58616b',
+      '--default-btn-text-color': '#fff',
+      '--default-btn-hover-bg': '#292d31',
+      '--default-btn-border-color': '#343a40',
+      '--default-btn-focus-box-shadow':
+        '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+      '--danger-btn-bg': '#b52d3a',
+      '--danger-btn-text-color': '#fff',
+      '--danger-btn-hover-bg': '#88222c',
+      '--danger-btn-border-color': '#88222c',
+      '--input-border-color': '#ced4da',
+      '--input-bg-color': '#fff',
+      '--input-text-color': '#495057',
+      '--input-focus-bg-color': '#fff',
+      '--input-focus-text-color': '#495057',
+      '--input-focus-border-color': '#78818a',
+      '--input-focus-box-shadow': '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+    },
+    light: {
+      '--base-text-color': '#212529',
+      '--header-bg': '#fff',
+      '--header-text-color': '#212529',
+      '--default-btn-bg': '#fff',
+      '--default-btn-text-color': '#212529',
+      '--default-btn-hover-bg': '#e8e7e7',
+      '--default-btn-border-color': '#343a40',
+      '--default-btn-focus-box-shadow':
+        '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+      '--danger-btn-bg': '#f1b5bb',
+      '--danger-btn-text-color': '#212529',
+      '--danger-btn-hover-bg': '#ef808a',
+      '--danger-btn-border-color': '#e2818a',
+      '--input-border-color': '#ced4da',
+      '--input-bg-color': '#fff',
+      '--input-text-color': '#495057',
+      '--input-focus-bg-color': '#fff',
+      '--input-focus-text-color': '#495057',
+      '--input-focus-border-color': '#78818a',
+      '--input-focus-box-shadow': '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+    },
+  };
+  let lastSelectedTheme = "default";
+
   // UI Elements
   const listContainer = document.querySelector(".tasks-list-section .list-group");
   
@@ -44,9 +112,12 @@ const tasks = [
   const inputTitle = form.elements['title'];
   const inputBody = form.elements['body'];
 
+  const themeSelect = document.getElementById('themeSelect');
+
   renderAllTasks(objOfTasks);
-  form.addEventListener("submit", FormSubmitHandler);
-  listContainer.addEventListener("click", DeleteHandler);
+  form.addEventListener("submit", formSubmitHandler);
+  listContainer.addEventListener("click", deleteHandler);
+  themeSelect.addEventListener("change", themeSelectHandler);
 
   function renderAllTasks(tasksList) {
     if (!tasksList) {
@@ -86,7 +157,7 @@ const tasks = [
     return li;
   }
 
-  function FormSubmitHandler(e) {
+  function formSubmitHandler(e) {
     e.preventDefault();
     const titleValue = inputTitle.value;
     const bodyValue = inputBody.value;
@@ -124,17 +195,35 @@ const tasks = [
     return isConfirmed;
   }
 
-  function DeleteTaskFromHtml(isConfirmed, el) {
+  function deleteTaskFromHtml(isConfirmed, el) {
     if (!isConfirmed) return;
     el.remove();
   }
 
-  function DeleteHandler({ target }) {
+  function deleteHandler({ target }) {
     if (target.classList.contains("delete-btn")) {
       const parent = target.closest("[data-task-id]");
       const id = parent.dataset.taskId;
       const isConfirmed = deleteTask(id);
-      DeleteTaskFromHtml(isConfirmed, parent);
+      deleteTaskFromHtml(isConfirmed, parent);
     }
+  }
+
+  function themeSelectHandler(e) {
+    const selectedTheme = themeSelect.value;
+    const isConfirmed = confirm(`Do you really want to change the theme to ${selectedTheme}?`);
+    if (!isConfirmed) {
+      themeSelect.value = lastSelectedTheme;
+      return;
+    }
+    setTheme(selectedTheme);
+    lastSelectedTheme = selectedTheme;
+  }
+
+  function setTheme(theme) {
+    const selectedThemeObj = themes[theme];
+    Object.entries(selectedThemeObj).forEach(([key, value]) => {
+      document.documentElement.style.setProperty(key, value);
+    });
   }
 })(tasks);
